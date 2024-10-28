dd88016c4a41d495bd49bb65b0f363268fde5787
Tony Bui <Trung.bui26@gmail.com>
2024-10-29 08:17:15 +1100

    add navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
